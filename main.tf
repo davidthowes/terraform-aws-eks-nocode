b799ea1ec00648292972ef2346d54dbacbcbcf30
davidthowes <david.howes@hashicorp.com>
2023-06-22 16:11:31 -0400

    Update main.tf
    added var.cluster_name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "${var.cluster_name}-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
