e67dacbde3286fdefca6f12e184c237b9c74e6f3
davidthowes <david.howes@hashicorp.com>
2023-06-23 09:20:02 -0400

    Update variables.tf with catch all

--- variables.tf
+++ variables.tf
@@ -10,3 +10,8 @@ variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
+
+variable "anything_you_want" {
+  description = "Additional variables such as node group size, ami, machine type, etc."
+  type        = string
+}
